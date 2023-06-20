8447c33456485af3ccf97e18cbd8422f59044001
cedric kadima <kadimacedric3@gmail.com>
2023-06-20 16:32:40 +0200

    creation personnage

--- back_end/game.py
+++ back_end/game.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 import asyncio
 import websockets
+from back_end.classe.CharacterClass import Character
+
+if __name__ == "__main__":
+    main()
 
 async def handler(websocket):
     while True:
@@ -8,6 +12,8 @@ async def handler(websocket):
         print(message)
 
 async def main():
+    Player = Character("Player", 100, 10, 10, 10)
+    print(f"Player a {Player.health} points de vie.")
     async with websockets.serve(handler, "", 8001):
         await asyncio.Future()  # run forever
 
