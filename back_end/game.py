5f0c7ba4dbc2005399bb5185dba30dea19c38bc1
cedric kadima <kadimacedric3@gmail.com>
2023-06-21 15:17:22 +0200

    changement de message socket

--- back_end/game.py
+++ back_end/game.py
@@ -1,21 +1,55 @@
-#!/usr/bin/env python
+# #!/usr/bin/env python
+# import asyncio
+# import websockets
+# from classe.CharacterClass import Character
+# from classe.MapClass import Map
+# import json
+#
+# async def handler(websocket):
+#     while True:
+#         message = await websocket.recv()
+#         print(message)
+#
+#         NewMap = Map(10, 10)
+#         await websocket.send(json.dumps({'NewMap': NewMap}))
+#
+# async def main():
+#     async with websockets.serve(handler, "", 8002):
+#         await asyncio.Future()  # run forever
+#
+# if __name__ == "__main__":
+#     asyncio.run(main())
+
 import asyncio
+import json
+
 import websockets
-from back_end.classe.CharacterClass import Character
 
-if __name__ == "__main__":
-    main()
+from back_end.classe.MapClass import Map
 
-async def handler(websocket):
-    while True:
-        message = await websocket.recv()
-        print(message)
 
-async def main():
-    Player = Character("Player", 100, 10, 10, 10)
-    print(f"Player a {Player.health} points de vie.")
-    async with websockets.serve(handler, "", 8001):
-        await asyncio.Future()  # run forever
+async def handle_client(websocket, path):
+    try:
+        while True:
+            message = await websocket.recv()
+            print("Received from client:", message)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+            # Envoyer une réponse au client
+            response = "Hello from server!"
+            await websocket.send(response)
+
+            #Envoyer une map au client
+            Newmap = Map(10, 10)
+            await websocket.send(json.dumps(Newmap.__dict__))
+
+    except websockets.ConnectionClosed:
+        print("Client disconnected")
+
+async def start_server():
+    server = await websockets.serve(handle_client, "localhost", 8001)
+    print("Server started and listening on ws://localhost:8001")
+
+    # Garder le serveur en cours d'exécution
+    await server.wait_closed()
+
+asyncio.run(start_server())
