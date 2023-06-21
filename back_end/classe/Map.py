4dd9de69e8dbcd51ac1ac135fbc8b01f5e69402e
Arod Alexandre <arod.alexandre.contact@gmail.com>
2023-06-21 16:02:39 +0200

    try map

--- back_end/classe/Map.py
+++ back_end/classe/Map.py
@@ -1,6 +1,13 @@
+import random
+
 class Map:
     def __init__(self, lines, columns):
-        self.grid = []
+        self.grid = {}
         for i in range(lines):
             for j in range(columns):
-                self.grid.append([i, j])
+                self.grid[(i, j)] = None
+
+    def populate(self, events):
+        for line in range(len(self.grid)):
+            for column in range(len(self.grid[line])):
+                self.grid[(line, column)] = random.choice(events)
--- back_end/game.py
+++ back_end/game.py
@@ -3,6 +3,7 @@ import asyncio
 import websockets
 import json
 from classe.Player import Player
+from classe.Map import Map
 
 async def handler(websocket):
     player = Player("John", 100, 50, 20, 30)
@@ -10,6 +11,10 @@ async def handler(websocket):
         await websocket.send(json.dumps(player.__dict__))
 
 async def main():
+    map = Map(10, 10)
+    print(map.grid)
+#     map.populate(['1', '2', '3'])
+    print(map.grid[(2, 2)])
     async with websockets.serve(handler, "", 8001):
         print("Server started at port 8001")
         await asyncio.Future()  # run forever
