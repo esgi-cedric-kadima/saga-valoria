2325783e9d28c7a407260f8eb5c161b6fab38043
Arod Alexandre <arod.alexandre.contact@gmail.com>
2023-06-21 16:30:14 +0200

    Fix map : je sais pas faire un tableau en python :'(

--- back_end/classe/Map.py
+++ back_end/classe/Map.py
@@ -2,12 +2,14 @@ import random
 
 class Map:
     def __init__(self, lines, columns):
-        self.grid = {}
+        self.grid = []
         for i in range(lines):
+            row = []
             for j in range(columns):
-                self.grid[(i, j)] = None
+                row.append(None)
+            self.grid.append(row)
 
     def populate(self, events):
         for line in range(len(self.grid)):
             for column in range(len(self.grid[line])):
-                self.grid[(line, column)] = random.choice(events)
+                self.grid[line][column] = random.choice(events)
