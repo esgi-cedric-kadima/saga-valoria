5f71318e876c9b956f33591a83ce1af905583b06
Arod Alexandre <arod.alexandre.contact@gmail.com>
2023-06-21 15:08:25 +0200

    Test sockets

--- back_end/game.py
+++ back_end/game.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python
 import asyncio
 import websockets
-from back_end.classe.CharacterClass import Character
-
-if __name__ == "__main__":
-    main()
+import json
+from classe.Player import Player
 
 async def handler(websocket):
-    while True:
-        message = await websocket.recv()
-        print(message)
+    player = Player("John", 100, 50, 20, 30)
+    async for message in websocket:
+        await websocket.send(json.dumps(player.__dict__))
 
 async def main():
-    Player = Character("Player", 100, 10, 10, 10)
-    print(f"Player a {Player.health} points de vie.")
     async with websockets.serve(handler, "", 8001):
+        print("Server started at port 8001")
         await asyncio.Future()  # run forever
 
 if __name__ == "__main__":
